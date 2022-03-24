19f206b8cf8b9074561762f1bf30c576bfb228ab
CrowdedAstronaut <ross@johnrossphillips.com>
2022-03-24 08:11:20 -0500

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Ross Phillips on 3/24/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
