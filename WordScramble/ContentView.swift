4537ee2a766e99620d09beb8044a1982018cda81
CrowdedAstronaut <ross@johnrossphillips.com>
2022-03-24 09:26:42 -0500

    Working with strings

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,10 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+	let people = ["Finn", "Leia", "Luke", "Rey"]
+
+	var body: some View {
+		Text("Hello World")
+			.padding()
+
+	}
+	
+	func test(){
+		let word = "swift"
+		let checker = UITextChecker()
+		
+		let range = NSRange(location: 0, length: word.utf16.count)
+		
+		let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+		
+		let allGood = misspelledRange.location == NSNotFound
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
