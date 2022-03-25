f2f8131a102a296c84fd47f726330d3d94448f12
CrowdedAstronaut <ross@johnrossphillips.com>
2022-03-25 07:47:39 -0500

    Adding to a list of words

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,23 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
-	let people = ["Finn", "Leia", "Luke", "Rey"]
+	@State private var usedWords = [String]()
+	@State private var rootWord = ""
+	@State private var newWord = ""
 
 	var body: some View {
-		Text("Hello World")
-			.padding()
+		NavigationView {
+			List {
+				Section {
+					TextField("Enter your word", text: $newWord)
+						.autocapitalization(.none)
+				}
 
+				Section {
+					ForEach(usedWords, id: \.self) { word in
+						HStack {
+							Image(systemName: "\(word.count).circle")
+							Text(word)
+						}
+					
+					}
+				}
+			}
+			.navigationTitle(rootWord)
+			.onSubmit(addNewWord)
+		}
 	}
-	
-	func test(){
-		let word = "swift"
-		let checker = UITextChecker()
-		
-		let range = NSRange(location: 0, length: word.utf16.count)
-		
-		let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
-		
-		let allGood = misspelledRange.location == NSNotFound
+	func addNewWord() {
+		// lowercase and trim the word, to make sure we don't add duplicate words with case differences
+		let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+
+		// exit if the remaining string is empty
+		guard answer.count > 0 else { return }
+
+		// extra validation to come
+
+		usedWords.insert(answer, at: 0)
+		newWord = ""
 	}
 }
 
